da7a5635b224873ec54d58095dbf66a619090692
apollo_jhn <japollo.ponteras@gmail.com>
2025-03-15 01:57:10 +0800

    Python API, Assets and Dynamic Loading

--- js/index.js
+++ js/index.js
@@ -1,4 +1,30 @@
 "use strict";
+// Constants
+const _APP = document.getElementById("app");
+const PAGES_PATH = "pages/";
+const PAGES_EXTENSION = ".html";
+const PAGES = {
+    1: "homepage",
+    2: "buy_water",
+    3: "deposit_bottle",
+    4: "about",
+    5: "pricing",
+    6: "stats",
+    7: "conclusion"
+};
+// Onload
 window.onload = () => {
     console.log("Hello world");
 };
+// Load HTML page to App
+function loadToApp() {
+}
+// Load pages Function
+function loadPage(targetElement, page_name, page_path = PAGES_PATH, page_fileExtention = PAGES_EXTENSION) {
+    const path = page_path + page_name + page_fileExtention;
+    targetElement.innerHTML = "";
+    fetch(path)
+        .then(response => response.text())
+        .then(data => targetElement.innerHTML = data)
+        .catch(error => console.error('Error loading content: ', error));
+}
--- ts/index.ts
+++ ts/index.ts
@@ -1,3 +1,33 @@
+// Constants
+const _APP: HTMLElement | null = document.getElementById("app")
+const PAGES_PATH: string = "pages/"
+const PAGES_EXTENSION: string = ".html"
+const PAGES = {
+  1:"homepage",
+  2:"buy_water",
+  3:"deposit_bottle",
+  4:"about",
+  5:"pricing",
+  6:"stats",
+  7:"conclusion"
+}
+
+// Onload
 window.onload = () => {
   console.log("Hello world")
+}
+
+// Load HTML page to App
+function loadToApp () {
+
+}
+
+// Load pages Function
+function loadPage (targetElement: HTMLElement, page_name: string, page_path: string = PAGES_PATH, page_fileExtention = PAGES_EXTENSION) {
+  const path = page_path + page_name + page_fileExtention;
+  targetElement.innerHTML = "";
+  fetch(path)
+      .then(response => response.text())
+      .then(data => targetElement.innerHTML = data)
+      .catch(error => console.error('Error loading content: ', error));
 }
